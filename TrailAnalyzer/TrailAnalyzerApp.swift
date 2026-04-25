1da628003cbbd02cc2754096dd53bd8666b8bd2d
soraya-ire <rujiangkongshi@gmail.com>
2026-04-25 22:22:08 +0900

    Initial Commit

---
+++ TrailAnalyzer/TrailAnalyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrailAnalyzerApp.swift
+//  TrailAnalyzer
+//
+//  Created by soraya irie on 2026/04/25.
+//
+
+import SwiftUI
+
+@main
+struct TrailAnalyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
