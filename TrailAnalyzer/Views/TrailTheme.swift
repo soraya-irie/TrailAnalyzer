ecd5d652494f4285488e987dd9d00b4fa9ed82e9
soraya-ire <rujiangkongshi@gmail.com>
2026-04-27 14:51:51 +0900

    Complete Section5 Step2 - import models with core ml

--- TrailAnalyzer/Views/TrailTheme.swift
+++ TrailAnalyzer/Views/TrailTheme.swift
@@ -1,7 +1,18 @@
-import Foundation
+import SwiftUI
 
 struct TrailTheme: ViewModifier {
     func body(content: Content) -> some View {
         content
     }
+}
+
+extension View {
+    func trailTheme() -> some View {
+        modifier(TrailTheme())
+    }
+}
+
+#Preview {
+    Text("Hello, World!")
+        .trailTheme()
 }
