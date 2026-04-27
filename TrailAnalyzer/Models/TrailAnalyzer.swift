69de678dcb73e048cfff10986041eb396da76cbe
soraya-ire <rujiangkongshi@gmail.com>
2026-04-27 13:33:23 +0900

    Complete Section3 Step2 - import models with core ml

--- TrailAnalyzer/Models/TrailAnalyzer.swift
+++ TrailAnalyzer/Models/TrailAnalyzer.swift
@@ -1,7 +1,13 @@
 import Foundation
+import CoreML
 
 struct TrailAnalyzer {
     func predictRisk() -> Risk {
+        do {
+            let model = try TrailAnalyzerModel(configuration: MLModelConfiguration())
+        } catch {
+
+        }
         return .highRisk
     }
 }
