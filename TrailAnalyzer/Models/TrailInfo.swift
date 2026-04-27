9bb35fba09470f571579dea32f62fc2333f964fe
soraya-ire <rujiangkongshi@gmail.com>
2026-04-27 14:07:35 +0900

    Complete Section4 Step4 - import models with core ml

--- TrailAnalyzer/Models/TrailInfo.swift
+++ TrailAnalyzer/Models/TrailInfo.swift
@@ -4,5 +4,8 @@ struct TrailInfo {
     var distance: Int?
     var elevation: Int?
     var terrain: Terrain?
-    var wildlifeDangerLevel: Int
+    var wildlifeDangerLevel: Int = lowDanger
+
+    static let lowDanger = 0
+    static let highDanger = 1
 }
