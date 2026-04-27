92bb5330256464e01b0f050b6311e586c4bfe7fe
soraya-ire <rujiangkongshi@gmail.com>
2026-04-27 16:42:06 +0900

    Complete Section7 Step6 - import models with core ml

--- TrailAnalyzer/Views/ContentView.swift
+++ TrailAnalyzer/Views/ContentView.swift
@@ -35,6 +35,7 @@ struct ContentView: View {
             .navigationTitle("Trail Analyzer")
             .trailTheme()
         }
+        .tint(Color.trailTheme)
         .onAppear {
             trailInfo = .sample
         }
