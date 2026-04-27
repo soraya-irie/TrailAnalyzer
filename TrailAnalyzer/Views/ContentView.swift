7c3e5e5695a500905c222e47f85574f885b086e2
soraya-ire <rujiangkongshi@gmail.com>
2026-04-27 16:04:08 +0900

    Complete Section6 Step4 - import models with core ml

--- TrailAnalyzer/Views/ContentView.swift
+++ TrailAnalyzer/Views/ContentView.swift
@@ -9,13 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ScrollView {
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Hello, world!")
+            }
+            .padding()
         }
-        .padding()
+        .trailTheme()
     }
 }
 
