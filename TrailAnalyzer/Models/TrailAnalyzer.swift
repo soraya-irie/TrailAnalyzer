12e1669c435ea326602b2fa017e672b820e73e49
soraya-ire <rujiangkongshi@gmail.com>
2026-04-27 13:36:01 +0900

    Complete Section3 Step3 - import models with core ml

--- TrailAnalyzer/Models/TrailAnalyzer.swift
+++ TrailAnalyzer/Models/TrailAnalyzer.swift
@@ -5,6 +5,13 @@ struct TrailAnalyzer {
     func predictRisk() -> Risk {
         do {
             let model = try TrailAnalyzerModel(configuration: MLModelConfiguration())
+
+            let input = TrailAnalyzerModelInput(
+                distance: 1,
+                elevation: 0,
+                terrain: "paved",
+                dangerous: 0
+            )
         } catch {
 
         }
