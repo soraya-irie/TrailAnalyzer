2372340dcf2148946361e399c347871c2a5c9c53
soraya-ire <rujiangkongshi@gmail.com>
2026-04-27 13:20:55 +0900

    Complete Section2 Step8 - import models with core ml

--- TrailAnalyzer/Views/RiskCard.swift
+++ TrailAnalyzer/Views/RiskCard.swift
@@ -4,7 +4,15 @@ struct RiskCard: View {
     @State var risk: Risk
 
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Image(risk.image)
+                .resizable()
+                .frame(width: 30, height: 30)
+            Text(risk.rawValue)
+                .font(.title2)
+        }
+        Text(risk.description)
+            .frame(maxWidth: .infinity, alignment: .leading)
     }
 }
 
