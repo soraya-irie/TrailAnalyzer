c25084964dd6d0948991227f4ac4352be9b64c36
soraya-ire <rujiangkongshi@gmail.com>
2026-04-27 15:32:27 +0900

    Complete Section5 Step11 - import models with core ml

--- TrailAnalyzer/Views/TrailInfoView.swift
+++ TrailAnalyzer/Views/TrailInfoView.swift
@@ -5,9 +5,10 @@ struct TrailInfoView: View {
 
     var body: some View {
         VStack {
-            Text("Distance")
-            TextField("kilometers", value: $trailInfo.distance, format: .number)
-                .keyboardType(.numberPad)
+            TrailField(iconName: "figure.hiking", label: "Distance") {
+                TextField("kilometers", value: $trailInfo.distance, format: .number)
+                    .keyboardType(.numberPad)
+            }
 
             Text("Elevation Change")
             TextField("meters", value: $trailInfo.elevation, format: .number)
