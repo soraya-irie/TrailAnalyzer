218b1ab2bd32b8b1e7abaa04b09e7deba48e43fb
soraya-ire <rujiangkongshi@gmail.com>
2026-04-27 16:03:04 +0900

    Complete Section6 Step3 - import models with core ml

--- TrailAnalyzer/Views/RiskCard.swift
+++ TrailAnalyzer/Views/RiskCard.swift
@@ -21,6 +21,6 @@ struct RiskCard: View {
 }
 
 #Preview {
-    let prediction = TrailAnalyzer().predictRisk()
+    let prediction = TrailAnalyzer().predictRisk(trailInfo: .sample)
     RiskCard(risk: prediction)
 }
